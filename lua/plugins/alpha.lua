af65ce771cc62e8ccad6e949dd874db7df5f988e
ochi12 <kaurichannel@gmail.com>
2024-03-20 00:45:23 +0800

    x

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -1,37 +1,41 @@
 return {
-	"goolord/alpha-nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons", "nvim-lua/plenary.nvim" },
-	config = function()
-		local alpha = require("alpha")
-		local dashboard = require("alpha.themes.dashboard")
-		local theta = require("alpha.fortune")
+  "goolord/alpha-nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons", "nvim-lua/plenary.nvim" },
+  config = function()
+    local alpha = require("alpha")
+    local dashboard = require("alpha.themes.dashboard")
+    local theta = require("alpha.fortune")
 
-		local green = vim.g.terminal_color_2
-		local orange = vim.g.terminal_color_9
-		vim.api.nvim_set_hl(0, "YonvimDashboardLogo1", { fg = green })
-		vim.api.nvim_set_hl(0, "YonvimDashboardLogo2", { fg = orange })
- 
-		local logo = {
-      [[         ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄        ]],
-      [[       ▄▀                ▀▄      ]],       
-      [[       █                  █      ]],
-			[[       █         ▄▀▀▄     █▄▀▀▄  ]],
-      [[       █         █   ▀▄▄▄▄▀   █  ]],
-      [[       █        █             ▀▄ ]],
-      [[       █        █   ▄█   ▄ ▄█  █ ]],
-      [[       █        █    ▄  ▄  ▄   █ ]],
-      [[       ▀▄        ▀▄  ▀▀▀▀▀▀▀ ▄▀  ]],-- ▄▀█ 
-      [[         ▀▀▀▀▀▀▀▀▀▀█▀▀█▀█▀▀█▀    ]],
-      [[                    ▀▀▀  ▀▀      ]]
-	  }
-		dashboard.section.header.val = logo
+    local green = vim.g.terminal_color_2
+    local orange = vim.g.terminal_color_9
+    vim.api.nvim_set_hl(0, "YonvimDashboardLogo1", { fg = green })
+    vim.api.nvim_set_hl(0, "YonvimDashboardLogo2", { fg = orange })
 
-		dashboard.section.header.opts.hl = "YonvimDashboardLogo2"
-		dashboard.section.footer.val = theta()
+    local logo = {
+      [[ ███       ███ ]],
+      [[████      ████]],
+      [[██████     █████]],
+      [[███████    █████]],
+      [[████████   █████]],
+      [[█████████  █████]],
+      [[█████ ████ █████]],
+      [[█████  █████████]],
+      [[█████   ████████]],
+      [[█████    ███████]],
+      [[█████     ██████]],
+      [[████      ████]],
+      [[ ███       ███ ]],
+      [[                  ]],
+      [[ N  E  O  V  I  M ]],
+    }
+    dashboard.section.header.val = logo
 
-		alpha.setup(dashboard.opts)
-		vim.cmd([[
+    dashboard.section.header.opts.hl = "YonvimDashboardLogo2"
+    dashboard.section.footer.val = theta()
+
+    alpha.setup(dashboard.opts)
+    vim.cmd([[
       autocmd FileType alpha setlocal nofoldenable
     ]])
-	end,
+  end,
 }
--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -10,12 +10,8 @@ return {
 		vim.opt.termguicolors = true
 		vim.keymap.set("n", "<C-n>", ":Neotree source=filesystem action=focus position=left toggle=true<CR>")
 
-		vim.keymap.set("n", "<leader>1", ":Neotree source=filesystem action=focus position=left toggle=true<CR>")
-		vim.keymap.set("n", "<leader>2", ":Neotree source=buffers action=focus position=left toggle=true<CR>")
-		vim.keymap.set("n", "<leader>3", ":Neotree source=git_status action=focus position=left toggle=true<CR>")
 
-		-- colors
-		vim.api.nvim_set_hl(0, "unfocused", { fg = "#282828" })
+  
 
 		require("neo-tree").setup({
 			close_if_last_window = true,
